b855aa32810eb01d51d201425bc826b43de2c41c
Lacerda003 <132013520+Lacerda003@users.noreply.github.com>
2023-06-10 13:09:45 -0300

    Definição do estoque

--- produto.hpp
+++ produto.hpp
@@ -33,10 +33,19 @@ public:
     friend class Produto;
     bool incluir_estoque(Produto produto);
 private:
-    std::set<std::string, Produto>estoque_nome;
-    std::map<float, Produto>estoque_valor;
+    //map pq cada nome deve ser único e ordenado por nome
+    std::map<std::string, Produto>estoque_nome;
+    //multmap pq podem existir vários produtos com o mesmo preço
+    std::multimap<float, Produto>estoque_valor;
+    //map pq cada código deve ser único e ordenado por códgio
     std::map<std::string, Produto>estoque_codigo;
-    std::map<int, std::string>estoque_quantidade;
+    //multimap pq podem existir vários intens com a mesma quantidade de unidades
+    std::multimap<int, Produto>estoque_quantidade;
+
+    std::map<std::string, Produto>::iterator it_nome = estoque_nome.begin();
+    std::multimap<float, Produto>::iterator it_valor = estoque_valor.begin();
+    std::map<std::string, Produto>::iterator it_codigo = estoque_codigo.begin();
+    std::multimap<int, Produto>::iterator it_quantidade = estoque_quantidade.begin();
 };
 
 #endif
