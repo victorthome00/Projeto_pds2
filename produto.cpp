f7bcbefe43e99d80e4b90d9ae31571f1a5ae416c
Lacerda003 <132013520+Lacerda003@users.noreply.github.com>
2023-06-27 13:14:39 -0300

    função para remover produto

--- produto.cpp
+++ produto.cpp
@@ -40,6 +40,7 @@ Produto::Produto(std::string nome_produto, float valor, std::string descricao, s
     _codigo_produto = codigo_produto;
     _quantidade = quantidade;
 }
+
 bool Estoque::incluir_estoque(Produto produto){
     // Verificar se o produto já existe no estoque pelo código
     if (estoque_codigo.find(produto._codigo_produto) != estoque_codigo.end()) {
@@ -52,6 +53,27 @@ bool Estoque::incluir_estoque(Produto produto){
     estoque_valor.insert(std::make_pair(produto._valor, produto));
     estoque_codigo.insert(std::make_pair(produto._codigo_produto, produto));
     estoque_quantidade.insert(std::make_pair(produto._quantidade, produto));
-    std::cout<<"Produto inserido com Sucesso"<<std::endl;
-    return true; // Produto incluído com sucesso no estoque
+
+    if (estoque_codigo.find(produto._codigo_produto) != estoque_codigo.end()) {
+        std::cout<<"Produto inserido com sucesso"<<std::endl;
+        return true; //Produto inserido com sucesso
+    }
+}
+bool Estoque::remover_produto(const std::string& codigo) {
+    // Verificar se o produto existe no estoque pelo código
+    auto it = estoque_codigo.find(codigo);
+    if (it == estoque_codigo.end()) {
+        std::cout << "Produto não existe" << std::endl;
+        return false; // Produto não encontrado no estoque
+    }
+    
+    // Remover o produto de todos os mapas
+    estoque_nome.erase(it->second.get_nome());
+    estoque_valor.erase(it->second.get_valor());
+    estoque_quantidade.erase(it->second.get_quantidade());
+    
+    estoque_codigo.erase(it);
+    
+    std::cout << "Produto removido com sucesso do estoque" << std::endl;
+    return true; // Produto removido com sucesso do estoque
 }
