e20f5dc3e7fcd96e26c56849234ced1bd36fc3c1
Victor Thome <victor-thome@hotmail.com>
2023-06-28 15:17:49 -0300

    att

--- usuario.hpp
+++ usuario.hpp
@@ -4,32 +4,58 @@
 #include <vector>
 #include <utility>
 #include <map>
+#include <iostream>
 
+class Estoque;
+class Carrinho; 
 class Usuario{
 public:
-    Usuario(std::string login, std::string senha, std::string endereco,
-            std::string codigo_usuario);
+    Usuario(const std::string login, const std::string senha, const std::string endereco,
+            const std::string codigo_usuario, const std::string nome_usuario);
+    virtual void trocar_senha();
+    virtual void trocar_end();
+    virtual void gerador_codigo();
+    virtual ~Usuario();
 
+protected:
     std::string login() const;
     std::string senha() const;
-    virtual std::string endereco() const;
-    virtual ~Usuario();
+    std::string endereco() const;
+    std::string nome_usuario() const;
 };
 
 
 class Cliente : public Usuario{
 public:
-    Cliente(std::string login, std::string senha, std::string endereco, 
-            std::string codigo_usuario);
+    friend class Carrinho;
+    Cliente(std::string login, std::string senha, std::string endereco, std::string codigo_usuario,
+            std::string nome_usuario);
+    void trocar_senha();
+    void trocar_end();
+    void gerador_codigo();
 
 private:
+    std::string login() const;
+    std::string nome_usuario() const;
+    std::string senha() const;
+    std::string endereco() const;
 };
 
 
 class Loja : public Usuario{
 public:
-    Loja(std::string login, std::string senha, std::string endereco,
-         std::string codigo_usuario);
+    friend class Estoque;
+    Loja(std::string login, std::string senha, std::string endereco, std::string codigo_usuario, 
+         std::string nome_usuario);
+    void trocar_senha();
+    void trocar_end();
+    void gerador_cogido();
+
+private:
+    std::string login() const;
+    std::string nome_usuario() const;
+    std::string senha() const;
+    std::string endereco() const;
 };
 
 #endif
