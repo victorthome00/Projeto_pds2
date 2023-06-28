7efa659ae46f099629953a4bb0e5920590dea3b0
arthur_qu18 <tuquaresma@gmail.com>
2023-06-28 15:41:12 -0300

    cadastro

---
+++ registro.hpp
@@ -0,0 +1,14 @@
+#ifndef REGISTRO_HPP
+#define REGISTRO_HPP
+
+#include <string>
+
+class Registro {
+public:
+    void cadastrarCliente();
+    void cadastrarLoja();
+    std::string validar_senha();
+};
+
+
+#endif
