cf35625f9e653e96cf4244aeb3328680ff450a8b
Lacerda003 <132013520+Lacerda003@users.noreply.github.com>
2023-07-01 15:50:57 -0300

    esboço inicial main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "login.hpp"
 #include <iostream>
 
-int main() {
+/*int main() {
     Registro registro;
     Login login;
 
@@ -11,7 +11,7 @@ int main() {
     std::cout << "2 - Cadastrar Loja" << std::endl;
     std::cout << "3 - Fazer login do Cliente" << std::endl;
     std::cout << "4 - Fazer login da Loja" << std::endl;
-    std::cout << "Escolha uma opção: ";
+    std::cout << "Escolha uma opção: "<<std::endl;
     std::cin >> opcao;
 
     if (opcao == 1) {
@@ -37,4 +37,31 @@ int main() {
     }
 
     return 0;
+}
+*/
+
+int main(){
+    std::string comando;
+    std::cout<<"Digite 'encerrar' a qualquer momento para encerrar a sessão"<<est::endl;
+    do{
+        std::cout<<"Digite 1 para cliente"<<"\n"<<"Digite 2 para Gerente"<<std::endl;
+        std::cin>>comando;
+        std::cout<<'\n'<<std::endl;
+        if(comando == "1"){
+            //fazer login
+            Login::autenticarCliente();
+            //fazer cadastro
+            //chamar página cliente
+        }
+        if(comando == "2"){
+            //fazer login
+            Login::autenticarLoja();
+            //fazer cadastro
+            //chamar página loja
+        }
+        else{
+            std::cout<<"ERRO\ncomando não reconhecido"<<std::endl;
+        }
+    } while (comando != "encerrar");
+    
 }
