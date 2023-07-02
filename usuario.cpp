9033f0f59c92f24f8c0c437ca558a90ec98ac188
Victor Thome <victor-thome@hotmail.com>
2023-07-02 14:04:38 -0300

    att usuarios_main

--- usuario.cpp
+++ usuario.cpp
@@ -1,21 +1,37 @@
 #include "usuario.hpp"
-#include <random>
+#include <fstream>
 
-class Cliente : public Usuario{
-public:
-    Cliente cliente;
-    std::string gerador_codigo() override {
-        std::string alfabeto("0123456789abcdefghijklmnopqrstuvwxyz");
-        std::random_device rd;
-        std::mt19937 gerador(rd());
-        std::shuffle(alfabeto.begin(), alfabeto.end(), gerador);
-        return alfabeto.subalfabeto(0,10);
-    }
+
+void cliente_main(){
+    std::string comando_secundario;
+    while(comando_secundario != ("Sair" || "sair")){
+        std::cout << "1: Produtos \n2: Usuário \n3: Carrinho \n4: Compras" << std::endl;
+        std::cout << "Digite o número referente ao que gostaria de prosseguir" << std::endl;
+        std::cin >> comando_secundario;
+        switch(comando_secundario){
+            case "1":
+            //adicionar arquivo dos produtos
+                std::ifstream arquivo();
+                break;
+            case "2":
+                std::ifstream arquivo("usuariosCliente.txt", std::ios::binary | std::ios::in);
+                if (arquivo.is_open())
+                {
+                    std::string nomeArquivo, senhaArquivo, emailArquivo, cpfArquivo, cepArquivo;
+                    std::cout << "Usuario:\n" << nomeArquivo\n\n << "Email:\n" << emailArquivo\n\n <<  "Cpf:\n" <<
+                    cpfArquivo\n\n << "Cep\n" << cepArquivo << std::endl;
+                    cin <<    
+                }
+                break;
+            case "3":
+            //adicionar arquivo do carrinho    
+                break;
+            case "4":
+            //adicionar historico de compras
+                break;
+            default:
+                std::cout << "Comando inválido\n" << "Tente novamente" << std::endl;
+                break;
+        }
+    } 
 }
-
-Usuario::Usuario(std::string login, std::string senha, std::string endereco, std::string codigo_usuario,
-                 std::string nome_usuario) : _login(login), _senha(senha), _endereco(endereco),
-                 _codigo_usuario(codigo_usuario), _nome_usuario(nome_usuario) {}
-
-Cliente::Cliente(std::string login, std::string senha, std::string endereco, std::string codigo_usuario,
-                 std::string nome_usuario) : Usuario(login, senha, endereco, nome_usuario, codigo_usuario){}
