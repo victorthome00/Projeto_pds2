36d085dfa16b6bff68628327ac52998eb2ead74b
arthur_qu18 <tuquaresma@gmail.com>
2023-07-05 18:13:21 -0300

    main funfando ate o momento

--- login.cpp
+++ login.cpp
@@ -1,47 +1,69 @@
 #include "login.hpp"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 bool Login::autenticarCliente(std::string nome, std::string senha) {
-    std::string nome, senha;
-    std::cout << "Digite o nome de usuario: ";
-    std::cin >> nome;
-    std::cout << "Digite a senha: ";
-    std::cin >> senha;
     senha = encrypit(senha);
-    std::ifstream arquivo("usuariosCliente.txt", std::ios::binary | std::ios::in);
-    if (arquivo.is_open()) {
-        std::string nomeArquivo, senhaArquivo, emailArquivo, cpfArquivo, cepArquivo;
-        while (arquivo >> nomeArquivo >> senhaArquivo >> emailArquivo >> cpfArquivo >> cepArquivo) {
+    std::string linha;
+    std::ifstream arquivo("usuariosClientes.txt", std::ios::binary | std::ios::in);
+     if (arquivo.is_open()) {
+        std::string nomeArquivo, senhaArquivo, emailArquivo, cpfArquivo;
+            while (getline(arquivo, linha)) {
+                nomeArquivo = linha;
+                getline(arquivo, linha);
+                senhaArquivo = linha;
+                getline(arquivo, linha);
+                emailArquivo = linha;
+                getline(arquivo, linha);
+                cpfArquivo = linha;
+                
+                senhaArquivo.erase(0, senhaArquivo.find_first_not_of(" \t\r\n"));
+                 
             if (nome == nomeArquivo && senha == senhaArquivo) {
                 arquivo.close();
                 std::cout << "Login do cliente realizado com sucesso!" << std::endl;
                 return true;
             }
-        }
-    }
+            }
     arquivo.close();
     std::cout << "Falha na realizacao do login do cliente!" << std::endl;
+    std::cout << '\n';
     return false;
+}
 }
 
 bool Login::autenticarLoja(std::string nome, std::string senha){
     senha = encrypit(senha);
+    std::string linha;
 
-    std::ifstream arquivo("usuariosLojas.txt");
+    int i = 0;
+    std::ifstream arquivo("usuariosLoja.txt", std::ios::binary | std::ios::in);
     if (arquivo.is_open()) {
         std::string nomeArquivo, senhaArquivo, emailArquivo, cnpjArquivo, cepArquivo;
-        while (arquivo >> nomeArquivo >> senhaArquivo >> emailArquivo >> cnpjArquivo >> cepArquivo) {
+            while (getline(arquivo, linha)) {
+                nomeArquivo = linha;
+                getline(arquivo, linha);
+                senhaArquivo = linha;
+                getline(arquivo, linha);
+                emailArquivo = linha;
+                getline(arquivo, linha);
+                cnpjArquivo = linha;
+                getline(arquivo, linha);
+                cepArquivo = linha;
+                
+                senhaArquivo.erase(0, senhaArquivo.find_first_not_of(" \t\r\n"));
+                 
             if (nome == nomeArquivo && senha == senhaArquivo) {
                 arquivo.close();
                 std::cout << "Login da loja realizado com sucesso!" << std::endl;
                 return true;
             }
-        }
-        arquivo.close();
-    }
+            }
+    arquivo.close();
     std::cout << "Falha na realização do login do cliente!" << std::endl;
     return false;
+}
 }
 
 std::string Login::encrypit (std::string& senha) {
