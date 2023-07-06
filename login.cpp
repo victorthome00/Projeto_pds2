a447da4c64ef2c83895d244a6e5f8bf9cc6739ea
oranjestrawberry <maartenrouws@gmail.com>
2023-07-06 12:32:02 -0300

    dwa

--- login.cpp
+++ login.cpp
@@ -35,8 +35,6 @@ bool Login::autenticarCliente(std::string nome, std::string senha) {
 bool Login::autenticarLoja(std::string nome, std::string senha){
     senha = encrypit(senha);
     std::string linha;
-
-    //int i = 0;
     std::ifstream arquivo("usuariosLoja.txt", std::ios::binary | std::ios::in);
     if (arquivo.is_open()) {
         std::string nomeArquivo, senhaArquivo, emailArquivo, cnpjArquivo, cepArquivo;
